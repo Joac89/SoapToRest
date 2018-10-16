cebe6112fb4072e14bef734ed9b9c1868f29861d
Jesus Aguilar <joac.aguilar@gmail.com>
2018-10-16 11:07:57 -0500

    Base SoapToRest

---
+++ SoapToRestCore/SoapService/Calculator.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SoapService
+{
+	// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+	// NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+	public class Service1 : ICalculator
+	{
+		public Result Efficiency(Amdhal data)
+		{
+			// = (SPEEDUP / CORES ) * 100
+			var result = (data.Speedup / data.Core) * 100;
+
+			return new Result()
+			{
+				Calculated = Math.Round(result, 4)
+			};
+		}
+
+		public Result Speedup(Speedup data)
+		{
+			// = SERIAL+TIME / (TIME/CORES+SERIAL)
+			var result = (data.SerialTime + data.Time) / ((data.Time / data.Core) + data.SerialTime);
+
+			return new Result()
+			{
+				Calculated = Math.Round(result, 4)
+			};
+		}
+	}
+}
---
+++ SoapToRestCore/SoapService/ICalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SoapService
+{
+	// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+	[ServiceContract]
+	public interface ICalculator
+	{
+		[OperationContract]
+		Result Speedup(Speedup data);
+
+		[OperationContract]
+		Result Efficiency(Amdhal data);
+
+	}
+	
+	[DataContract]
+	public class Speedup
+	{
+		[DataMember]
+		public decimal Core { get; set; }
+		[DataMember]
+		public decimal Time { get; set; }
+		[DataMember]
+		public decimal SerialTime { get; set; }
+	}
+
+	[DataContract]
+	public class Amdhal
+	{
+		[DataMember]
+		public decimal Core { get; set; }
+		[DataMember]
+		public decimal Speedup { get; set; }
+	}
+
+	[DataContract]
+	public class Result
+	{
+		[DataMember]
+		public decimal Calculated { get; set; }
+	}
+}
